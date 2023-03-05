bae11742668e9af0010521c25603b1b70281ea04
Rasool65 <Aghajani.Rasool@Gmail.com>
2023-03-05 10:22:48 +0330

    initial project

---
+++ RasoolWebAuth/Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RasoolWebAuth.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? CredentialId { get; set; }
+        public string? PasswordlessPublicKey { get; set; }
+    }
+}
---
+++ RasoolWebAuth/Data/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RasoolWebAuth.Data
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string? Username { get; set; }
+    }
+}
